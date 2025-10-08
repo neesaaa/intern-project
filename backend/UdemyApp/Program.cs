d797c51b4a35fdcca2c352e7efd279a8345ad60b
Nassar <20014642eng.asu.edu.eg>
2025-10-08 18:16:27 +0300

    feat: swagger fix

--- backend/UdemyApp/Program.cs
+++ backend/UdemyApp/Program.cs
@@ -141,6 +141,7 @@ namespace UdemyApp
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "UdemyApp API v1");
+                    c.RoutePrefix = string.Empty;   
                 });
             }
             using var scope = app.Services.CreateScope();
